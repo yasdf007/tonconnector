113d015da7b49ef8083e689ee511b27c452258ce
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2022-04-22 19:59:48 +0300

    bot plays the game now

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # Main bot executable
 # (c) Bonzoteam 2022
 
-from discord import Intents
+from discord import Intents, Game, Status
 from discord.ext.commands import Bot as defaultBot, Cog
 from db import postgres
 
@@ -9,15 +9,18 @@ from dotenv import load_dotenv
 from os import getenv, listdir
 load_dotenv()
 
+
 class Bot(defaultBot):
     def __init__(self):
         intents = Intents.default()
-        self.ton_api_key = getenv("TONCENTERKEY") 
+        self.game = Game(f"=> verif help <=")
+
         super().__init__(
             command_prefix="verif ",
             help_command=None,
             intents=intents,
         )
+
     def Load(self):
         for filename in listdir("./commands"):
             if filename.endswith(".py"):
@@ -30,6 +33,7 @@ class Bot(defaultBot):
     @Cog.listener()
     async def on_ready(self):
         self.database = await postgres.connectToDB()
+        await self.change_presence(status=Status.online, activity=self.game)
         print('done loading')
 
 
